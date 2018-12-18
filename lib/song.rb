80c4017328f7bde43365c4f1e089f747b3cf799a
Tara Sabella <tarasabella@gmail.com>
2018-12-18 23:53:09 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,12 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  #class method that stores all instances of Song class that are created when saved 
+  def initialize(name, artist_name)
+    @name = name 
+    @artist_name = artist_name 
+    #all song intsances have basic properties of a name & artist name 
+  end 
 
   def self.all
     @@all
